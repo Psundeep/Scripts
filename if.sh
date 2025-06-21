b3b991b086cce7bdf362217ccc4a62cda782eeb0
kpasam <kpasam@KPASAM-IN.in.oracle.com>
2025-06-21 21:33:49 +0530

    if script check

---
+++ if.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter numberss"
+
+read -p "Enter your name: " a
+read -p "Enter your name: " b
+
+if [ $a gt $b ] then 
+  echo "a is greater than b"
+else 
+  echo "b is greater than a"
+
+fi 
+
+## script done
+   
