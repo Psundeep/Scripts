81a386337ca03c27425e714fe218274b98c6a41b
kpasam <kpasam@KPASAM-IN.in.oracle.com>
2025-06-21 21:16:36 +0530

    second script

--- second.sh
+++ second.sh
@@ -7,5 +7,8 @@ echo "enter names"
 read person1
 read person2
 
+echo "enter person1 : $person1"
+echo "enter person2 : $person2"
+
 echo "hello $person1 how are you"
 echo "hi $person2 im doing great how about you"
